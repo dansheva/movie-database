39626fc7cbd418c6ca36e63eed2c4066ea4423d2
dansheva <dansheva2@gmail.com>
2021-12-10 11:57:49 +0100

    app reducer logic

--- src/2-store/1-app-reducer/app-reducer.ts
+++ src/2-store/1-app-reducer/app-reducer.ts
@@ -1,10 +1,36 @@
-
-type AppReducerStateType = {}
-
-const initialState: AppReducerStateType = {}
-
-export const appReducer = (state = initialState, action: AppActionsTypes): AppReducerStateType => {
-    return state
+type AppReducerStateType = {
+    errors: string | null,
+    isLoading: boolean
 }
 
-type AppActionsTypes = any
+const initialState: AppReducerStateType = {
+    errors: null,
+    isLoading: false
+}
+
+export const appReducer = (state = initialState, action: AppActionsTypes): AppReducerStateType => {
+    switch (action.type) {
+        case "SET_ERROR":
+            return {...state, errors: action.err}
+        case "SET_IS_LOADING":
+            return {...state, isLoading: action.isLoading}
+        default:
+            return state
+    }
+}
+
+type AppActionsTypes = setIsLoadingActionType | SetErrorActionType
+
+type setIsLoadingActionType = ReturnType<typeof setIsLoadingAC>
+export const setIsLoadingAC = (isLoading: boolean) => ({
+        type: 'SET_IS_LOADING',
+        isLoading
+    } as const
+)
+
+type SetErrorActionType = ReturnType<typeof setErrorAC>
+export const setErrorAC = (err: string) => ({
+        type: 'SET_ERROR',
+        err
+    } as const
+)
